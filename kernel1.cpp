8bb516781a74f35a7761970a62ecf9ac5a73ad7d
Siddhant Varma <siddhantvarma@outlook.com>
2017-10-14 17:38:43 +0530

    prog no-2 completed

--- kernel1.cpp
+++ kernel1.cpp
@@ -11,7 +11,13 @@ int main(void){
 	system("cat /proc/cpuinfo |awk 'NR==3, NR==4{print}'\n");
 	cout<<"\nAmount of CPU time since system was last booted is:\n";
 	system("sudo cat /proc/uptime\n");
-	
+	cout<<"\nConfigured Memory is:\n";
+	system("cat /proc/meminfo | awk 'NR==1{print$2}'\n");
+	cout<<"\nAmout of Free Memory:\n";
+	system("cat /proc/meminfo | awk 'NR==2{print$2}'\n");
+	cout<<"\nAmount of Memory used:\n";
+	system("cat /proc/meminfo | awk '{if (NR==1) a = $2; if(NR==2) b = $2 }END {print a-b}'\n");
+
 	cout<<endl;
 	return 0;
 	
