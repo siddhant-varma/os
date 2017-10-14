4e88889e779aa42bbe79b3be14c1d91e45e295ff
Siddhant Varma <siddhantvarma@outlook.com>
2017-10-14 17:23:32 +0530

    initial commit for kernel program at no-2

---
+++ kernel1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+
+using namespace std;
+
+int main(void){
+	cout<<"\nThe kernel version is:\n";
+	system("cat /proc/sys/kernel/osrelease\n");
+	cout<<"\nCPU Space:\n";
+	system("cat /proc/cpuinfo |awk 'NR==3, NR==4{print}'\n");
+	cout<<"\nAmount of CPU time since system was last booted is:\n";
+	system("sudo cat /proc/uptime\n");
+	
+	cout<<endl;
+	return 0;
+	
+}
+	
