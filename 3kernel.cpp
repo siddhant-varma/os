4090c0f5b1c8c228e3afe74d6999d9baf79de4bf
Siddhant Varma <siddhantvarma@outlook.com>
2017-10-14 17:47:25 +0530

    prog no-3 added and working fine it was earlier a part of 2kernel.cpp

---
+++ 3kernel.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main(void){
+	cout<<"\nAmount of CPU time since system was last booted is:\n";
+	system("sudo cat /proc/uptime\n");
+	cout<<"\nConfigured Memory is:\n";
+	system("cat /proc/meminfo | awk 'NR==1{print$2}'\n");
+	cout<<"\nAmout of Free Memory:\n";
+	system("cat /proc/meminfo | awk 'NR==2{print$2}'\n");
+	cout<<"\nAmount of Memory used:\n";
+	system("cat /proc/meminfo | awk '{if (NR==1) a = $2; if(NR==2) b = $2 }END {print a-b}'\n");
+
+	cout<<endl;
+	return 0;	
+}
