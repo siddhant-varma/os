6de2176f7b9ad229c4292c4d84446dc9aec64e95
siddhant-varma <siddhantvarma@outlook.com>
2017-10-17 11:31:49 +0530

    data structure for process and process amanagement added with intilize()

--- 8scheduling.cpp
+++ 8scheduling.cpp
@@ -2,7 +2,43 @@
 
 using namespace std;
 
+class psManagement{
+	private:
+		int size;
+		
+		struct process{
+			int arrTime, burstTime, priority, id;
+			
+		} *ps;
+		
+	public:
+		psManagement(int s){
+			size = s;
+			ps = new process[size];
+		}
+		
+		void intilizie(int ch){
+			for(int i = 0; i < size; i++){
+				cout<<"\nFor Process "<<i+1<<" enter:\n\tId:\t";
+				cin>>ps[i].id;
+				cout<<"\tArrival Time:\t";
+				cin>>ps[i].arrTime;
+				cout<<"\tBurst Time:\t";
+				cin>>ps[i].burstTime;
+				/*cout<<"\tPriority:\t";
+				cin>>ps[i].priority;*/
+			}
+		}
+};
+
 int main(void){
+	int temp;
+	
+	cout<<"Enter Number of processes:\t";
+	cin>>temp;
+	
+	psManagement ps(temp);
+	ps.intilizie(temp);
 	
 	return 0;
 }
