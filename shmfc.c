84d98f2ec5cf237108b7e1dba074da10ef69e1fe
siddhant-varma <siddhantvarma@outlook.com>
2017-10-23 17:16:45 +0530

    shared memory programs

---
+++ shmfc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+int main(void){
+	const int SIZE = 4096;
+	const char *name = "OS";
+	const char *message_0 = "Message";
+	const char *message_1 = "Message Passed!";
+	int shm_fd;
+	char *ptr;
+	shm_fd = shm_open(name, O_CREAT|O_RDWR, 0666);
+	ftruncate(shm_fd, SIZE);
+	ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	printf("\nIn main\n");                         -
+	sprintf(ptr, "%s", message_0);
+	//ptr += strlen(message_0);
+	sprintf(ptr, "%s\n", message_1);
+	//ptr += strlen(message_1);
+	printf("%s", ptr);
+	return 0;
+}
